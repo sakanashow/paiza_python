b5709aa87bba22d6fb3b523f549a76f81c217d9f
sakana <mashou625@gmail.com>
2024-01-26 07:25:44 +0900

    2回目の回答

--- RankC/C013.py
+++ RankC/C013.py
@@ -1,17 +1,14 @@
 n = input()
 num = int(input())
 h = []
-x = 0
 for i in range(num):
     r = input()
-    #print(r)
     if n in r:
         continue
     else:
         h.append(r)
-        x += 1
-    
-if x == 0:
+      
+if not h: #リストが空のとき、Falseを、空でなければTrueを返す
     h.append('none')
 for ans in h:
     print(ans)
