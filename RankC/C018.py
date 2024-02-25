f9f7e19c2747599c44cf85b9e3b4534a21453fa2
mashou <mashou625@gmail.com>
2024-02-25 14:31:09 +0900

    C018を追加

--- RankC/C018.py
+++ RankC/C018.py
@@ -12,14 +12,43 @@ my = int(input())
 for a in range(my):
     name, number = (input().split())
     have[name] = int(number)
-# print(have)
+
+keys = []
 for n in menus:
-    # print(n)
-    # print(have[n])
-    # print(menus[n])
-    if(menus[n] // have[n] == 0):
-        print(menus[n])        
+    if n not in have: # そもそも持っている素材に必要素材が存在しないならその時点で0を出して終了
+        ans = 0
+        break
+    else: # それ以外の時、いくつ作れるか計算する
+        key = have[n] // menus[n] # それぞれの素材同士で割り算する
+        keys.append(key)
+        ans = min(keys) # ０がなければ全素材数のうち最小を出力
+print(ans)
 
+# リファクタリング
+recipe_count = int(input())
+menus = {}
+have = {}
+ans = float('inf')  # 無限大で初期化 無限大で初期化しておくと最小値がとりやすくなる
 
-# それぞれの素材同士で割り算して一つでも０があればその時点で０を出力
-# ０がなければ全素材数のうち最小を出力
+# レシピの入力
+for _ in range(recipe_count):
+    name, number = input().split()
+    menus[name] = int(number)
+
+# 手元の材料の入力
+my_ingredients_count = int(input())
+for _ in range(my_ingredients_count):
+    name, number = input().split()
+    have[name] = int(number)
+
+# レシピごとに計算
+for name, number in menus.items():
+    if name not in have:
+        ans = 0  # 手元に足りない材料がある場合、作れる料理の数は0
+        break
+    else:
+        # 作れる料理の数を計算し、最小値を更新
+        possible_dishes = have[name] // number
+        ans = min(ans, possible_dishes)
+
+print(ans)
