dc283bd0b6e9c3d7d7e550070ec79d2562e0fe2b
mashou <mashou625@gmail.com>
2024-06-20 23:02:03 +0900

    不要なファイルを削除

--- RankC/C020.py
+++
@@ -1,8 +0,0 @@
-def calculate_seisen(kg, row, seisen):
-    weight_after_row = kg * ((100 - row) * 0.01)
-    seisen_weight = weight_after_row * ((100 - seisen) * 0.01)
-    return seisen_weight
-
-kg, row, seisen = map(int, input().split())
-result = calculate_seisen(kg, row, seisen)
-print(result)
